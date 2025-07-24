402000e4683b0ec7921a2f4bd3914c9744891e80
Mashanz <mangatkk@gmail.com>
2025-07-24 19:48:54 +0700

    fix: logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<h1>Rupa Karya Kopi Pandeglang</h1>
+<svelte:head>
+    <title>Rupa Karya Kopi Pandeglang</title>
+</svelte:head>
+
+<main class="flex w-full h-screen bg-gray-100">
+    <div class="m-auto p-4">
+        <img style="width: 800px; heigh: 800px" class=" rounded-full" src="/logo-rupakarya.png" alt="">
+        <h1 class="text-center font-bold hidden">Rupa Karya Kopi Pandeglang</h1>
+    </div>
+</main>
